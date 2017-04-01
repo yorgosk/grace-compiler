5fe2e0d711d542dc92fe18c6aecf4f5f094c268e
yorgosk <kgeoinvest@gmail.com>
2017-04-01 10:45:15 +0300

    trying to make examples work without producing exception

--- src/main/java/compiler/Main.java
+++ src/main/java/compiler/Main.java
@@ -2,45 +2,47 @@
 package compiler;
 
 import compiler.lexer.Lexer;
-//import compiler.lexer.LexerException;
+import compiler.lexer.LexerException;
 import compiler.node.*;
+import compiler.parser.Parser;
+import compiler.parser.ParserException;
 
-//import java.io.IOException;
+import java.io.IOException;
 import java.io.PushbackReader;
 import java.io.InputStreamReader;
 
 class Main {
 
 	public static void main(String args[]) {
-		PushbackReader reader = new PushbackReader(new InputStreamReader(System.in));
-		Lexer lexer = new Lexer(reader);
-
-		for(;;) {
-			try {
-				Token t = lexer.next();
-
-				if (t instanceof EOF)
-					break;
-				System.out.println(t.toString());
-			} catch (Exception e)
-			{
-				System.err.println(e.getMessage());
-			}
-		}
-//		Start tree = null;
+//		PushbackReader reader = new PushbackReader(new InputStreamReader(System.in));
+//		Lexer lexer = new Lexer(reader);
 //
-//		PushbackReader reader = new PushbackReader(new InputStreamReader(System.in), 1024);
-//		try {
-//			Parser p = new Parser(new Lexer(reader));
-//			tree = p.parse();
-//		} catch (LexerException e) {
-//			System.err.printf("Lexing error: %s\n", e.getMessage());
-//		} catch (IOException e) {
-//			System.err.printf("I/O error: %s\n", e.getMessage());
-//			e.printStackTrace();
-//		} catch (ParserException e) {
-//			System.err.printf("Parsing error: %s\n", e.getMessage());
+//		for(;;) {
+//			try {
+//				Token t = lexer.next();
+//
+//				if (t instanceof EOF)
+//					break;
+//				System.out.println(t.toString());
+//			} catch (Exception e)
+//			{
+//				System.err.println(e.getMessage());
+//			}
 //		}
+		Start tree = null;
+
+		PushbackReader reader = new PushbackReader(new InputStreamReader(System.in), 1024);
+		try {
+			Parser p = new Parser(new Lexer(reader));
+			tree = p.parse();
+		} catch (LexerException e) {
+			System.err.printf("Lexing error: %s\n", e.getMessage());
+		} catch (IOException e) {
+			System.err.printf("I/O error: %s\n", e.getMessage());
+			e.printStackTrace();
+		} catch (ParserException e) {
+			System.err.printf("Parsing error: %s\n", e.getMessage());
+		}
 
 		System.exit(0);
 	}
