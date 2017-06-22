fb044049df0f8da7a78b4902bb2150ae3e606f3f
tiropit <tiropit@users.noreply.github.com>
2017-06-22 18:57:15 +0300

    stack crash solved

--- g.s
+++ g.s
@@ -1,6 +1,5 @@
 .intel_syntax noprefix # Use Intel syntax instead of AT&T
 .text
-<<<<<<< HEAD
 name(prime) proc near
 push bp
 mov bp, sp
@@ -51,65 +50,10 @@ endof(prime): mov sp, bp
 pop bp
 ret
 name(prime) endp
-=======
-name(reverse) proc near
-push bp
-mov bp, sp
-sub sp, size
-load(R, l)
-store(R, $t1)
-load(R, i)
-store(R, 0)
-load(ax, i)
-load(dx, l)
-cmp ax, dx
-instr label(?)
-load(ax, l)
-load(dx, i)
-sub ax, dx
-store(ax, $t2)
-load(ax, $t2)
-load(dx, 1)
-sub ax, dx
-store(ax, $t3)
-load(ax, s)
-mov cx, size
-imul cx
-loadAddr(cx, $t3)
-add ax, cx
-store(ax, $t4)
-load(ax, r)
-mov cx, size
-imul cx
-loadAddr(cx, i)
-add ax, cx
-store(ax, $t5)
-load(R, [$t5])
-store(R, [$t4])
-load(ax, i)
-load(dx, 1)
-add ax, dx
-store(ax, $t6)
-load(R, i)
-store(R, $t6)
-load(ax, r)
-mov cx, size
-imul cx
-loadAddr(cx, i)
-add ax, cx
-store(ax, $t7)
-load(R, [$t7])
-store(R, '\0')
-endof(reverse): mov sp, bp
-pop bp
-ret
-name(reverse) endp
->>>>>>> 131936fc1c64f8f138581500ebaec7e88730f920
 name(main) proc near
 push bp
 mov bp, sp
 sub sp, size
-<<<<<<< HEAD
 load(R, limit)
 store(R, $t6)
 load(R, counter)
@@ -186,8 +130,6 @@ add ax, dx
 store(ax, $t19)
 load(R, number)
 store(R, $t19)
-=======
->>>>>>> 131936fc1c64f8f138581500ebaec7e88730f920
 endof(main): mov sp, bp
 pop bp
 ret
