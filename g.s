328ae2a2f6169f51db3fe165ff9f6d3042bc161a
tiropit <tiropit@users.noreply.github.com>
2017-06-26 01:59:13 +0300

    more sem corrections7

--- g.s
+++ g.s
@@ -1,29 +1,47 @@
 .intel_syntax noprefix # Use Intel syntax instead of AT&T
 .text
-_wrong_size_1 proc near
-push bp
-mov bp, sp
-sub sp, 8
-add ax, dx
-mov size ptr [bp+offset], ax
-mov si, word ptr [bp+offset]
-mov size ptr [si], R
-ret
-_wrong_size_11: mov sp, bp
-pop bp
-ret
-_wrong_size_11 endp
-_main_12 proc near
+_reverse_1 proc near
 push bp
 mov bp, sp
 sub sp, 8
 push si
 push si
 sub sp, 2
-call near ptr wrong_size
+call near ptr strlen
 add sp, size+4
-_main_16: mov sp, bp
+mov size ptr [bp+offset], R
+mov size ptr [bp+offset], R
+cmp ax, dx
+jl ?
+jmp ?
+sub ax, dx
+mov size ptr [bp+offset], ax
+sub ax, dx
+mov size ptr [bp+offset], ax
+mov size ptr [bp+offset], R
+add ax, dx
+mov size ptr [bp+offset], ax
+mov size ptr [bp+offset], R
+jmp 17
+mov size ptr [bp+offset], R
+_reverse_19: mov sp, bp
 pop bp
 ret
-_main_16 endp
+_reverse_19 endp
+_main_20 proc near
+push bp
+mov bp, sp
+sub sp, 8
+push si
+sub sp, 2
+call near ptr reverse
+add sp, size+4
+push si
+sub sp, 2
+call near ptr puts
+add sp, size+4
+_main_25: mov sp, bp
+pop bp
+ret
+_main_25 endp
 .data
