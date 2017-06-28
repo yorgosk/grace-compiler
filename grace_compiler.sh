b9e974179a117b7fbbbf76a9f6f437b64482a814
yorgosk <kgeoinvest@gmail.com>
2017-06-28 14:20:04 +0300

    hello.grace produces assembly successfully... with shell script additions (III)

--- grace_compiler.sh
+++ grace_compiler.sh
@@ -21,9 +21,52 @@ elif [ $# -eq 2 ]
 		gcc -m32 -o executable g.s
 		./executable
 		rm executable
+	elif [ $2 = "--upto-ir" ]
+		then	
+		java -cp target/compiler-1.0-SNAPSHOT.jar compiler.Main $1
 	else
 		echo "Unsupported second argument!"
 		exit -2
+	fi
+elif [ $# -eq 3 ]
+	then
+	rebFlag=0
+	irFlag=0
+	if [ $2 = "--no-rebuild" ]
+		then
+		let "rebFlag = rebFlag+1"
+		java -cp target/compiler-1.0-SNAPSHOT.jar compiler.Main $1
+		gcc -m32 -o executable g.s
+		./executable
+		rm executable
+	elif [ $2 = "--upto-ir" ]
+		then
+		let "irFlag = irFlag+1"
+		java -cp target/compiler-1.0-SNAPSHOT.jar compiler.Main $1
+	else
+		echo "Unsupported second argument!"
+		exit -2
+	fi
+	if [ $2 = "--upto-ir" ]
+		then
+		let "rebFlag = rebFlag+1"
+		java -cp target/compiler-1.0-SNAPSHOT.jar compiler.Main $1
+		gcc -m32 -o executable g.s
+		./executable
+		rm executable
+	elif [ $2 = "--no-rebuild" ]
+		then
+		let "irFlag = irFlag+1"
+		java -cp target/compiler-1.0-SNAPSHOT.jar compiler.Main $1
+	else
+		echo "Unsupported second argument!"
+		exit -2
+	fi
+	
+	if (( rebFlag != 1 )) || (( irFlag != 1 ))	# if there is a flag missing
+	then
+		echo "ERROR! Bad patamerers!"
+		exit -1
 	fi
 else
 	echo "ERROR! Too many arguments!"
